bd26a914e90e0bc704a83e3020b4d526ffb13b8d
Ruchita Varma <ruchitavarma5-cloud>
2026-02-19 18:18:13 +0000

    firstcommit

---
+++ jenkins_install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install openjdk-21-jre -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+
+
